6e175541532dca95733792aaf452e6aad46905dc
Kulnifer <juliazh0u384@gmail.com>
2021-09-01 18:31:15 -0400

    create a new file with censored swear words in it

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 # this program will censor swearwords in a .txt file
 import string
 
-textList = ["HAVE YOU CHECK YOUR", "BUTTHOLE???"]
-swearList = ["fuck", "shit"]
+censor = "*"
+swearList = ["fuck", "shit", "bitch", "nigga", "cunt"]
+censoredList = []
 
-def writeLine(textList):
-    f = open("TextFiles/text1.txt", "wt")
+
+def write_line(text_list, file_name):
+    file_name = file_name + " (censored)"
+    f = open("TextFiles/" + file_name, "wt")
     f.write("censored ver: \n\n")
 
-    f = open("TextFiles/text1.txt", "at")
-    for x in textList:
-        f.write(x + "\n")
+    f = open("TextFiles/" + file_name, "at")
+    for x in text_list:
+        f.write(x)
+
+
+def censor_word(word):
+    censored_word = word
+    altword = word.translate(str.maketrans("", "", string.punctuation))
+    for swearWord in swearList:
+        if swearWord in altword.lower():
+            censored_word = word[:2] + censor + word[3:]
+    return censored_word
 
 
 # read each line and then each word
 def read_word():
+    f = open("TextFiles/text1.txt", "rt")
     for line in f:
         for word in line.split():
             # print(word)
-            altword = word.translate(str.maketrans("","",string.punctuation))
-            if altword.lower() == swearList[0]:
-                print(word)
+            censoredWord = censor_word(word)
+            # replace swearword with censored swearword
+            line = line.replace(word, censoredWord)
+        censoredList.append(line)
+    f.close()
 
 
-f = open("TextFiles/text1.txt", "rt")
-
-#for line in f:
-    #print(line)
 read_word()
+write_line(censoredList, "text1.text")
 
-
-
-f.close()
